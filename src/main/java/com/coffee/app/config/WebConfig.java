497318c577920fcf17ab8862e2eadff3a648e5c1
uusuri <kunitski@mail.ru>
2025-12-05 14:02:54 +0300

    adolf coffee

--- src/main/java/com/coffee/app/config/WebConfig.java
+++ src/main/java/com/coffee/app/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000");
+                        .allowedOrigins("*")   // разрешаем все источники для теста
+                        .allowedMethods("*");
             }
         };
     }
